a9d728da7daebbeb13e28c95d19d4d1d949d44ed
Grinmak <grinmak@gmail.com>
2022-06-29 11:13:19 +0300

    add removeGrid function to clear grid area after entering new prompt value

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-
-    
-    let gridMult;
-
+    let gridMult = 0;
     let gridItems; 
     let gridWidth;
-
+    //button
     const btnSection = document.createElement('section');
     const gridBtn = document.createElement('button');
     gridBtn.classList.add('button');
-    
+    // new grid area
     const mainArea = document.createElement('div');
     const body = document.querySelector('body');
     gridBtn.textContent = 'Create grid!';
-    
+    //button placement
     btnSection.appendChild(gridBtn); 
     body.appendChild(btnSection);
     body.appendChild(mainArea);
@@ -20,13 +17,24 @@
     mainArea.setAttribute('id', 'grid')
     mainArea.setAttribute('class', 'mainArea');
 
+
+    function removeGrid () {
+        const gridRemove = document.getElementById('grid').querySelectorAll('div');
+        for (let i = 0; i < gridRemove.length; i++){
+            gridRemove[i].remove();
+        }
+    }
+
     function userInput() {
         gridBtn.addEventListener('click', function askUser() {
+            if (gridMult > 0) {
+                    removeGrid();
+                };
             let input =  prompt('Choose grid size');
             if (input <= 100){
                 gridMult = Number(input);
                 gridItems = Math.pow(gridMult, 2);
-                gridWidth = Number((100/gridMult).toFixed(2));
+                gridWidth = Number((100/gridMult).toFixed(2) - 0.01);
                 grid16();
             }else alert('Try number less than 100');
         });
@@ -36,11 +44,14 @@
     userInput();
 
     function grid16 (){
+
+
         for(let i=0; i<gridItems; i++){
             const onePiece = document.createElement('div');
             onePiece.setAttribute('class', 'gridDivs');
             onePiece.setAttribute('style', `min-width: ${gridWidth}%`);
             mainArea.appendChild(onePiece);
+            console.log(gridWidth);
         } 
         const allDivs = document.getElementById('grid').querySelectorAll('div');
         allDivs.forEach(div => div.addEventListener('mouseover', changeColor));
@@ -50,5 +61,5 @@
 
     function changeColor (){
         this.classList.add('changeColor');
+        
     };
-    
